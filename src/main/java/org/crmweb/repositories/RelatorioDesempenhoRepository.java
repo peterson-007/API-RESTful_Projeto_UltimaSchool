b06cb4745f42411e854864425c25c66ecf2139ac
peterson-007 <peterson_gs@hotmail.com>
2023-12-18 16:16:53 -0300

    first commit

--- src/main/java/org/crmweb/repositories/RelatorioDesempenhoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.crmweb.repositories;
-
-import org.crmweb.models.RelatorioDesempenho;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RelatorioDesempenhoRepository extends JpaRepository<RelatorioDesempenho, Long> {
-}
